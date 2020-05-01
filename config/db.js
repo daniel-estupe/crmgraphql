2333abaee4ed0eb1c6803955d341c0474055a903
Daniel Estupe <estupe.gonzalez@gmail.com>
2020-04-30 23:04:07 -0600

    Connection to MongoDB stablished.

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'variables.env' });
+
+const conectarDB = async () => {
+	try {
+		await mongoose.connect(process.env.DB_MONGO, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		});
+
+		console.log('Se ha establecido conexión a la base de datos.');
+	} catch (error) {
+		console.log('Hubo un error al conectarse a la base de datos.', error);
+		process.exit(1);
+	}
+};
+
+module.exports = conectarDB;
