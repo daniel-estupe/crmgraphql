0614cc861dbf5a0676a2ea309d6ccf351b40fa0e
Daniel Estupe <estupe.gonzalez@gmail.com>
2020-05-02 01:00:31 -0600

    User mutation added.

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,6 +1,33 @@
+const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
+
 const resolvers = {
 	Query: {
 		getClient: () => 'My first client.'
+	},
+	Mutation: {
+		nuevoUsuario: async (_, { input }) => {
+			const { email, password } = input;
+
+			// revisar si el usuario ya está registrado
+			const existeUsuario = await Usuario.findOne({ email });
+			if (existeUsuario) {
+				throw new Error('El usuario ya está registrado.');
+			}
+
+			// hashear su password
+			const salt = await bcryptjs.genSalt(10);
+			input.password = await bcryptjs.hash(password, salt);
+
+			// guardarlo en la base de datos
+			try {
+				const usuario = new Usuario(input);
+				usuario.save();
+				return usuario;
+			} catch (error) {
+				console.log(error);
+			}
+		}
 	}
 };
 
--- db/schema.js
+++ db/schema.js
@@ -1,8 +1,27 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
+	type Usuario {
+		id: ID
+		nombre: String
+		apellido: String
+		email: String
+		creado: String
+	}
+
+	input UsuarioInput {
+		nombre: String!
+		apellido: String!
+		email: String!
+		password: String!
+	}
+
 	type Query {
 		getClient: String
+	}
+
+	type Mutation {
+		nuevoUsuario(input: UsuarioInput): Usuario
 	}
 `;
 
