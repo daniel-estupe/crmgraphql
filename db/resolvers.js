f5f34efafe2ea8b83166c8f6e02ca4c2b60e8a70
Daniel Estupe <estupe.gonzalez@gmail.com>
2020-05-02 23:43:45 -0600

    Authentication and get user actions added.

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,9 +1,22 @@
 const Usuario = require('../models/Usuario');
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config({ path: 'variables.env' });
+
+const crearToken = (usuario, secret, expiresIn) => {
+	const { id, email, nombre, apellido } = usuario;
+
+	return jwt.sign({ id, email }, secret, { expiresIn });
+};
 
 const resolvers = {
 	Query: {
-		getClient: () => 'My first client.'
+		obtenerUsuario: async (_, { token }) => {
+			const usuarioid = await jwt.verify(token, process.env.SECRET);
+
+			return usuarioid;
+		}
 	},
 	Mutation: {
 		nuevoUsuario: async (_, { input }) => {
@@ -27,6 +40,26 @@ const resolvers = {
 			} catch (error) {
 				console.log(error);
 			}
+		},
+		autenticarUsuario: async (_, { input }) => {
+			const { email, password } = input;
+
+			// si el usuario existe
+			const existeUsuario = await Usuario.findOne({ email });
+			if (!existeUsuario) {
+				throw new Error('El usuario no existe.');
+			}
+
+			// revisar si el password es correcto
+			const passwordCorrecto = await bcryptjs.compare(password, existeUsuario.password);
+			if (!passwordCorrecto) {
+				throw new Error('La contraseña es incorrecta.');
+			}
+
+			// crear el token
+			return {
+				token: crearToken(existeUsuario, process.env.SECRET, '24h')
+			};
 		}
 	}
 };
